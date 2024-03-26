6f31cbcb4a80ab385533bc7b5e5e8084d8156a14
ibrahimdans <ibrahima.dansoko@outlook.com>
2024-03-26 17:55:44 +0100

    chore: add prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,17 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {MetaFunction} from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        {title: "New Remix App"},
+        {name: "description", content: "Welcome to Remix!"},
+    ];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-      <div className='m-6'> toto</div>
-    </div>
-  );
+    return (
+        <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
+            <h1 className="text-3xl font-bold underline">Welcome to world</h1>
+            <div className='m-6'> toto</div>
+        </div>
+    );
 }
